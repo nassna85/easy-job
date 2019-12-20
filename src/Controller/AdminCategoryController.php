100e03e5d29347d6167163a01c1347d28373616b
Naïm <nassna85@hotmail.fr>
2019-12-20 16:39:06 +0100

    Edition du nom de la catégorie cote admin

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -8,6 +8,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -55,6 +56,37 @@ class AdminCategoryController extends AbstractController
         }
         return $this->render('admin/category/new.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/administration/categorie/{slug}/{id}/modifier", name="admin_category_edit")
+     * @IsGranted("ROLE_ADMIN")
+     * @param Category $category
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Category $category, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($category);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "La catégorie a bien été modifiée."
+            );
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('admin/category/edit.html.twig', [
+            'form' => $form->createView(),
+            'category' => $category
         ]);
     }
 }
